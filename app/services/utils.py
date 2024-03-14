2c8c84735bb0ed6b1d99c73c657b7daa5ce3d92b
toir02 <wend8765@gmail.com>
2024-03-14 19:10:44 +0300

    added executable path to chromedriver

--- app/services/utils.py
+++ app/services/utils.py
@@ -2,12 +2,19 @@ import asyncio
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 
 
 async def get_page_content(path) -> str:
     chrome_options = Options()
-    chrome_options.add_argument("--headless")
-    driver = webdriver.Chrome(options=chrome_options)
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--disable-dev-shm-usage')
+    service = Service(executable_path="/usr/local/bin/chromedriver")
+    driver = webdriver.Chrome(
+        options=chrome_options,
+        service=service
+    )
 
     try:
         driver.get(f"https://blackrussia.online/{path}/")
