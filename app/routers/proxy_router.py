64a41afa6c5b5ca9a4c9d0ff193c82c56cc532e3
toir02 <wend8765@gmail.com>
2024-03-14 13:25:59 +0300

    added endpoint for parse html content

--- app/routers/proxy_router.py
+++ app/routers/proxy_router.py
@@ -1,6 +1,9 @@
+import re
+
 from fastapi import APIRouter, HTTPException
 
 from app.schemas.proxy_schemas import ProxyRequest
+from app.services.utils import get_page_content
 
 router = APIRouter()
 
@@ -14,3 +17,14 @@ async def method_not_allowed(path: str = ProxyRequest) -> None:
         status_code=405,
         detail="method not allowed"
     )
+
+
+@router.get("/{path:path}")
+async def get_html_content(
+        path: str = ProxyRequest
+) -> dict[str, str]:
+    content = await get_page_content(path)
+    content = re.sub(r'(?i)Black\s*Russia', 'BlackHub Games', content)
+    return {
+        "message": content
+    }
